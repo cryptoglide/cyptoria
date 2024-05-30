2cf9c8517c8f909c96c354e533e2cf99f8791462
cryptoglide <glide972@gmail.com>
2024-05-30 14:00:01 +0200

    Improve multi-chain contract deployment

--- token_minting.sol
+++ token_minting.sol
@@ -1,3 +1,4 @@
 // Add governance proposal expiration feature
 // Add security checks for contract deployment
 // Integrate multi-chain liquidity pools
+// Refactor contract to use EIP-712 signatures
