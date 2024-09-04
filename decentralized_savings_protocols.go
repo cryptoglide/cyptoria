5506fd4421d14eecc6de05a58fd4a51d518ef060
cryptoglide <glide972@gmail.com>
2024-09-04 14:00:02 +0200

    Refactor multi-signature wallet code

--- decentralized_savings_protocols.go
+++ decentralized_savings_protocols.go
@@ -1,3 +1,4 @@
 // Improve decentralized exchange (DEX) liquidity
 // Implement adaptive block size for Ethereum
 // Enhance governance contract flexibility
+// Fix vulnerabilities in staking contracts
