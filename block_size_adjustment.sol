5c1959148c7f28bcddada2cd16e98e71ccec9b68
cryptoglide <glide972@gmail.com>
2024-09-10 14:00:00 +0200

    Add non-fungible token (NFT) support

--- block_size_adjustment.sol
+++ block_size_adjustment.sol
@@ -1,3 +1,4 @@
 // Improve decentralized identity (DID) systems
 // Add support for Layer-2 solutions
 // Improve decentralized lending contract flexibility
+// Add native support for staking rewards
